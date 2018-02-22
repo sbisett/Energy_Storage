acace5f792a1f9b7c57d53800891b7e11ed840e1
kaylakelley <36654471+kaylakelley@users.noreply.github.com>
2018-02-22 16:14:58 -0600

    Molten Salt Java Upload

---
+++ Salt_Tanks_Main.java
@@ -0,0 +1,343 @@
+/* This calculates the temperature/volume of the hot and cold tanks for 3 temperature ranges and 3 heat transfer 
+ * coefficients. 
+ */
+
+package pkg2salt;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author erich
+ */
+public class Salt_Tanks_Main extends Application 
+{
+    
+    @Override
+    public void start(Stage primaryStage) 
+    {
+
+            Stage fig2=new Stage();
+            Stage fig3=new Stage();
+            Stage fig4=new Stage();
+            
+            // These parameters are the inputs
+            final double TANK_RADIUS=6.;  // m - both tanks have same dimensions
+            final double TANK_HEIGHT=6;  // m  
+            
+            final double STARTING_HOT_TANK_VOL_FRACT=0.75;  // total fluid volume equals the volume of one tank.  This is the fraction of the total fluid that's initially in the hot tank
+
+            final double P_IN = 50.E6;  // J/s - rate at which THERMAL power is transferred in when charging
+            final double P_OUT = 30.E6; // J/s - rate at which THERMAL power is transferred out when discharging
+            
+            //make these arrays
+            double T_HL[] = new double[3];
+            double T_CL[] = new double[3];
+            
+            T_HL[0] = 550;
+            T_HL[1] = 525;
+            T_HL[2] = 500;
+            
+            T_CL[0] = 350;
+            T_CL[1] = 375;
+            T_CL[2] = 400;
+            
+            double T_inf = 300.;  // K - the ambient temperature outside the tank
+            double hx_cf[] = new double[] {20, 35, 50}; // heat transfer coeff for natural cirulation convective heat xfer to environment - W/m2/K
+
+            String salt = "Solar Salt"; //Solar Salt, Hitec XL or Glauber
+            double c_p = 0;  // J/kg-K - default properties are water (an amazing version of water that never changes phase!)
+            double rho = 0; // kg/m3
+           
+            if (salt.equals("Solar Salt"))
+            {
+            		c_p = 1496;
+            		rho = 1899;
+            }
+            else if(salt.equals("Hitec XL"))
+            {
+	            	c_p = 1447;
+	        		rho = 1992;
+            }
+            else if(salt.equals("Glauber"))
+            {
+	            	c_p = 3349;
+	        		rho = 1460;
+            }
+            else 
+            {
+            		System.out.println("Error in salt input");
+            }
+   
+	            int N_EVENTS = 8;  // remember to change this; also remember java indexes arrays starting at 0
+	            int[] event = new int[N_EVENTS];
+	            double[] endt_event = new double[N_EVENTS];
+	            int[] tsteps_per_event = new int[N_EVENTS];
+	
+	            event[0]=0;  // discharge
+	            event[1]=1;  // do nothing for a while
+	            event[2]=2;  // charge
+	            event[3]=1;
+	            event[4]=0;
+	            event[5]=2;
+	            event[6]=1;
+	            event[7]=0;
+	            
+	            // can string together as many events 0, 1 and 2 as you want, but code will give garbage results if either tank becomes totally empty
+	            // or if heat loss to enviornment is too high
+	            // this can be fixed with some more coding but for now just keep an eye on the plots
+	            
+	            endt_event[0]=1800.;  // event 1 ends after 1800 s (30 minutes)
+	            endt_event[1]=3600.;  // event 2 ends at 3600 s (hence lasts 1800 s)
+	            endt_event[2]=5400.;
+	            endt_event[3]=12000.;
+	            endt_event[4]=15000.;
+	            endt_event[5]=17600.;
+	            endt_event[6]=36000.;
+	            endt_event[7]=38400.;
+	
+	            double dt = 50.;  // time step in seconds - I haven't tested convergence, could possibly jack this way up
+	
+	            // end of input section (and comments for the most part)
+	            
+	            int i,j,k;
+	            final double TANK_TOTVOL = Math.PI*TANK_RADIUS*TANK_RADIUS*TANK_HEIGHT;
+	            final double TANK_SIDESA = 2*Math.PI*TANK_RADIUS*TANK_HEIGHT;
+	            
+	            int nt = 0;
+	            
+	            tsteps_per_event[0]=(int)Math.round(endt_event[0]/dt);
+	            endt_event[0]=dt*(double)tsteps_per_event[0];
+	            nt = tsteps_per_event[0];
+		            for(i=1; i<N_EVENTS; i++) 
+		            {
+		                tsteps_per_event[i]=(int)Math.round((endt_event[i]-endt_event[i-1])/dt);
+		                endt_event[i]=endt_event[i-1]+dt*(double)tsteps_per_event[i];
+		                nt=nt+tsteps_per_event[i];
+		            }//for int i 
+	            
+	            double[] t = new double[nt];
+	            t[0]=0.;
+		            for (i=1; i<nt; i++) 
+		            {
+		                t[i]=t[i-1]+dt;
+		            }//for int i
+		            
+		        double[] Th = new double[nt];
+	            double[] Tc = new double[nt];
+	            double[] Vh = new double[nt];
+	            double[] Vc = new double[nt];
+	            double[] mh = new double[nt];
+	            double[] mc = new double[nt];
+	            //double[] mdotchg = new double[nt];
+	            //double[] mdotdis = new double[nt];
+		            
+		        double [][] Th_temp = new double[9][nt];
+		        double [][] Tc_temp = new double[9][nt]; 
+		        double [][] Vh_temp = new double[9][nt];
+		        double [][] Vc_temp = new double[9][nt]; 
+	       
+		        int count = 0;
+		        
+		    for(int h=0; h<3; h++)
+	        {
+		    	
+		        for(int g=0; g<3; g++) 
+		        {   
+		            Th[0]=T_HL[g];
+		            Tc[0]=T_CL[g];
+		            mh[0]=TANK_TOTVOL*rho*STARTING_HOT_TANK_VOL_FRACT;
+		            mc[0]=TANK_TOTVOL*rho*(1.-STARTING_HOT_TANK_VOL_FRACT);
+		            Vh[0]=mh[0]/rho;
+		            Vc[0]=mc[0]/rho;
+		           //mdotchg[0]=0.;
+		           //mdotdis[0]=0.;
+		            int ix = 1;
+		            double mt;
+		                    
+		            for(i=0; i<N_EVENTS; i++)  
+		            {
+			            switch (event[i]) 
+			            {
+			            case 0:  // discharge
+			                     for(j=0; j<tsteps_per_event[i]; j++) 
+			                     {
+			                         Th[ix]=Th[ix-1]-dt*(hx_cf[h]*(Th[ix-1]-T_inf)*TANK_SIDESA/(c_p*rho*Vh[ix-1]));
+			                         Tc[ix]=Tc[ix-1]-dt*(hx_cf[h]*(Tc[ix-1]-T_inf)*TANK_SIDESA/(c_p*rho*Vc[ix-1]));
+			                         Vh[ix]=Vh[ix-1]-dt*(P_OUT/(rho*c_p*(Th[ix-1]-T_CL[g]))); 
+			                         Vc[ix]=Vc[ix-1]+dt*(P_OUT/(rho*c_p*(Th[ix-1]-T_CL[g])));
+			                         mt=dt*(P_OUT/(rho*c_p*(Th[ix-1]-T_CL[g])));
+			                         ix++;
+			                     }//for int j
+			                     ix--;
+			                break;
+			
+			            case 1:  // just heat loss
+			                    for(j=0; j<tsteps_per_event[i]; j++) 
+			                    {
+			                         Th[ix]=Th[ix-1]-dt*(hx_cf[h]*(Th[ix-1]-T_inf)*TANK_SIDESA/(c_p*rho*Vh[ix-1]));
+			                         Tc[ix]=Tc[ix-1]-dt*(hx_cf[h]*(Tc[ix-1]-T_inf)*TANK_SIDESA/(c_p*rho*Vc[ix-1]));
+			                         Vh[ix]=Vh[ix-1];
+			                         Vc[ix]=Vc[ix-1];
+			                         ix++;
+			                     }//for int j
+			                    ix--;
+			                     break;
+			            case 2:  // charge
+			                     for(j=0; j<tsteps_per_event[i]; j++) 
+			                     {
+			                         Th[ix]=Th[ix-1]+dt*(P_IN*(T_HL[g]-Th[ix-1])/(Vh[ix-1]*c_p*rho*(T_HL[g]-Tc[ix-1]))-hx_cf[h]*(Th[ix-1]-T_inf)*TANK_SIDESA/(c_p*rho*Vh[ix-1]));//**
+			                         Tc[ix]=Tc[ix-1]-dt*(P_IN*(T_HL[g]-Th[ix-1])/(Vh[ix-1]*c_p*rho*(T_HL[g]-Tc[ix-1]))+hx_cf[h]*(Tc[ix-1]-T_inf)*TANK_SIDESA/(c_p*rho*Vc[ix-1]));//**
+			                         Vh[ix]=Vh[ix-1]+dt*(P_IN/(rho*c_p*(T_HL[g]-Tc[ix-1])));
+			                         Vc[ix]=Vc[ix-1]-dt*(P_IN/(rho*c_p*(T_HL[g]-Tc[ix-1]))); 
+			                         ix++;
+			                     }//for int j
+			                     break;
+			            default: // shouldn't be here
+			                     break;
+			            }//switch 
+		            }//for int i
+		            
+		            		for(int r=0; r<nt; r++)
+			            {
+			            	Th_temp[count][r] = Th[r];
+			            	Tc_temp[count][r] = Tc[r];
+			            	Vh_temp[count][r] = Vh[r];
+			            	Vc_temp[count][r] = Vc[r];
+			            }//for int r
+		            		
+		            		count ++;
+		            		
+	        		}//for int g
+            	}//for int h
+	        
+	        double [] Th1 = new double[nt];
+	        double [] Tc1 = new double[nt];
+	        double [] Vh1 = new double[nt]; 
+	        double [] Vc1 = new double[nt]; 
+	        
+	        double [] Th2 = new double[nt];
+	        double [] Tc2 = new double[nt]; 
+	        double [] Vh2 = new double[nt];
+	        double [] Vc2 = new double[nt];
+	        
+	        double [] Th3 = new double[nt];
+	        double [] Tc3 = new double[nt];
+	        double [] Vh3 = new double[nt]; 
+	        double [] Vc3 = new double[nt]; 
+	        
+	        double [] Th4 = new double[nt];
+	        double [] Tc4 = new double[nt];
+	        double [] Vh4 = new double[nt]; 
+	        double [] Vc4 = new double[nt]; 
+	        
+	        double [] Th5 = new double[nt];
+	        double [] Tc5 = new double[nt]; 
+	        double [] Vh5 = new double[nt];
+	        double [] Vc5 = new double[nt];
+	        
+	        double [] Th6 = new double[nt];
+	        double [] Tc6 = new double[nt];
+	        double [] Vh6 = new double[nt]; 
+	        double [] Vc6 = new double[nt]; 
+	        
+	        double [] Th7 = new double[nt];
+	        double [] Tc7 = new double[nt];
+	        double [] Vh7 = new double[nt]; 
+	        double [] Vc7 = new double[nt]; 
+	        
+	        double [] Th8 = new double[nt];
+	        double [] Tc8 = new double[nt]; 
+	        double [] Vh8 = new double[nt];
+	        double [] Vc8 = new double[nt];
+	        
+	        double [] Th9 = new double[nt];
+	        double [] Tc9 = new double[nt];
+	        double [] Vh9 = new double[nt]; 
+	        double [] Vc9 = new double[nt]; 
+	        
+	        for(int y=0; y<nt; y++)
+	        {
+	        		Th1[y]=Th_temp[0][y];
+	        		Tc1[y]=Tc_temp[0][y];
+	        		Vh1[y]=Vh_temp[0][y];
+	        		Vc1[y]=Vc_temp[0][y];
+	        		
+	        		Th2[y]=Th_temp[1][y];
+	        		Tc2[y]=Tc_temp[1][y];
+	        		Vh2[y]=Vh_temp[1][y];
+	        		Vc2[y]=Vc_temp[1][y];
+	        		
+	        		Th3[y]=Th_temp[2][y];
+	        		Tc3[y]=Tc_temp[2][y];
+	        		Vh3[y]=Vh_temp[2][y];
+	        		Vc3[y]=Vc_temp[2][y];
+	        		
+	        		Th4[y]=Th_temp[3][y];
+	        		Tc4[y]=Tc_temp[3][y];
+	        		Vh4[y]=Vh_temp[3][y];
+	        		Vc4[y]=Vc_temp[3][y];
+	        		
+	        		Th5[y]=Th_temp[4][y];
+	        		Tc5[y]=Tc_temp[4][y];
+	        		Vh5[y]=Vh_temp[4][y];
+	        		Vc5[y]=Vc_temp[4][y];
+	        		
+	        		Th6[y]=Th_temp[5][y];
+	        		Tc6[y]=Tc_temp[5][y];
+	        		Vh6[y]=Vh_temp[5][y];
+	        		Vc6[y]=Vc_temp[5][y];
+	        		
+	        		Th7[y]=Th_temp[6][y];
+	        		Tc7[y]=Tc_temp[6][y];
+	        		Vh7[y]=Vh_temp[6][y];
+	        		Vc7[y]=Vc_temp[6][y];
+	        		
+	        		Th8[y]=Th_temp[7][y];
+	        		Tc8[y]=Tc_temp[7][y];
+	        		Vh8[y]=Vh_temp[7][y];
+	        		Vc8[y]=Vc_temp[7][y];
+	        		
+	        		Th9[y]=Th_temp[8][y];
+	        		Tc9[y]=Tc_temp[8][y];
+	        		Vh9[y]=Vh_temp[8][y];
+	        		Vc9[y]=Vc_temp[8][y];
+	        		
+	        }// for y
+	        
+	       double [] yaxis1 = new double [] {400, 600};
+	       double [] yaxis2 = new double [] {300, 400};
+	       double [] yaxis3 = new double [] {100, 700};
+	       double [] yaxis4 = new double [] {0, 600};
+	             
+            XYPlot.MakePlot(primaryStage,t,Th1,t,Th2,t,Th3,t,Th4,t,Th5,t,Th6,t,Th7,t,Th8,t,Th9,"time[s]","Hot tank temperature [K]", "Hot Tank Temperature for "+ salt, yaxis1); 
+            primaryStage.show();
+            
+            XYPlot.MakePlot(fig2,t,Tc1,t,Tc2,t,Tc3,t,Tc4,t,Tc5,t,Tc6,t,Tc7,t,Tc8,t,Tc9,"time[s]","Cold tank temperature [K]", "Cold Tank Temperature for " + salt, yaxis2); 
+            fig2.show();
+            
+            XYPlot.MakePlot(fig3,t,Vh1,t,Vh2,t,Vh3, t,Vh4, t,Vh5, t,Vh6,t,Vh7,t,Vh8,t,Vh9,"time[s]","Hot tank fluid volume [m3]","Hot Tank Volume for " + salt, yaxis3); 
+            fig3.show();
+            
+            XYPlot.MakePlot(fig4,t,Vc1,t,Vc2,t,Vc3,t,Vc4,t,Vc5,t,Vc6,t,Vc7,t,Vc8,t,Vc9,"time[s]","Cold tank fluid volume [m3]","Cold Tank Volume for " + salt, yaxis4); 
+            fig4.show();
+           
+
+    }//public void start
+
+    
+    /**
+     * @param args the command line arguments
+     */
+	    public static void main(String[] args) 
+	    {
+	        launch(args);
+	    }
+	    
+}// public class main
---
+++ XYPlot.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2salt;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author erich
+ */
+public abstract class XYPlot extends Application {
+	
+    public static void MakePlot(Stage plot_stage, double[] xrange, double[] yrange, 
+    		double[] xrange2, double[] yrange2, 
+    		double[] xrange3, double[] yrange3, 
+    		double[] xrange4, double[] yrange4, 
+    		double[] xrange5, double[] yrange5, 
+    		double[] xrange6, double[] yrange6,
+    		double[] xrange7, double[] yrange7, 
+    		double[] xrange8, double[] yrange8,
+    		double[] xrange9, double[] yrange9,
+    		String xlabel, String ylabel, String title, double[] yaxis) 
+    {
+	    int l,m,n;
+	    double max_x, max_y=0.;
+	    n=yrange.length;
+	     
+	     for (l=0; (l<(n-1)); l++) 
+	     {
+	        if(yrange[l]>max_y) 
+	        {
+	            max_y = yrange[l] + 100; //change if not in the range when plotted
+	        }//if 
+	     }// for l
+	     
+	     max_x=xrange[xrange.length-1];
+	     final NumberAxis x_axis = new NumberAxis(0,max_x, max_x/10.);
+	     final NumberAxis y_axis = new NumberAxis (yaxis[0], yaxis[1], yaxis[1]/10.); //change if not in the range when plotted
+	     final ScatterChart<Number,Number> sc = new ScatterChart<Number,Number>(x_axis,y_axis);
+	     x_axis.setLabel(xlabel);
+	     y_axis.setLabel(ylabel);
+	     XYChart.Series s1 = new XYChart.Series();
+	     XYChart.Series s2 = new XYChart.Series();
+	     XYChart.Series s3 = new XYChart.Series();
+	     XYChart.Series s4 = new XYChart.Series();
+	     XYChart.Series s5 = new XYChart.Series();
+	     XYChart.Series s6 = new XYChart.Series();
+	     XYChart.Series s7 = new XYChart.Series();
+	     XYChart.Series s8 = new XYChart.Series();
+	     XYChart.Series s9 = new XYChart.Series();
+	     
+	     sc.setTitle(title);
+	     s1.setName("550K, 350K, 20W/m2/K");
+	     s2.setName("525K, 375K, 20W/m2/K");
+	     s3.setName("500K, 400K, 20W/m2/K");
+	     s4.setName("550K, 350K, 35W/m2/K");
+	     s5.setName("525K, 375K, 35W/m2/K");
+	     s6.setName("500K, 400K, 35W/m2/K");
+	     s7.setName("550K, 350K, 50W/m2/K");
+	     s8.setName("525K, 375K, 50W/m2/K");
+	     s9.setName("500K, 400K, 50W/m2/K");
+	     
+	     for (m=0; m<xrange.length-1; m++) 
+	     {
+	       s1.getData().add(new XYChart.Data(xrange[m],yrange[m]));
+	       s2.getData().add(new XYChart.Data(xrange2[m],yrange2[m]));
+	       s3.getData().add(new XYChart.Data(xrange3[m],yrange3[m]));
+	       s4.getData().add(new XYChart.Data(xrange4[m],yrange4[m]));
+	       s5.getData().add(new XYChart.Data(xrange5[m],yrange5[m]));
+	       s6.getData().add(new XYChart.Data(xrange6[m],yrange6[m]));
+	       s7.getData().add(new XYChart.Data(xrange7[m],yrange7[m]));
+	       s8.getData().add(new XYChart.Data(xrange8[m],yrange8[m]));
+	       s9.getData().add(new XYChart.Data(xrange9[m],yrange9[m]));
+	       
+	     }// for m
+	     
+	     sc.getData().addAll(s1, s2, s3,s4,s5,s6,s7,s8,s9);
+	     Scene scene  = new Scene(sc, 500, 400);
+	     plot_stage.setScene(scene);
+	     
+    }//public
+    
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+}
